8db705b16f469a734709ccf3873ffc85cf5a3676
Sabrina Akter <sabrinahossain202@gmail.com>
2023-08-23 21:26:07 +0600

    New folder named 'OOP' added inside the folder 'OOP'

---
+++ OOP/OOP/tutorial 12.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Details
+{
+    public:
+        void myFunc()
+        {
+            cout << "Hello World!";
+        }
+};
+
+int main()
+{
+    Details obj;
+    obj.myFunc();
+    return 0;
+}
