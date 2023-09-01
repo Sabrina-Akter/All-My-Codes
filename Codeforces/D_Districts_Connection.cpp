f45d5f64abd689c70fcb64f2894967979b64032f
Sabrina Akter <sabrinahossain202@gmail.com>
2023-09-01 11:37:37 +0600

    Codeforces/D_Districts_Connection.cpp

---
+++ Codeforces/D_Districts_Connection.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastt ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long int
+#define vll vector <ll>
+#define pb push_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ms(x) memset(x, 0, sizeof x)
+#define pll pair<ll,ll>
+#define plll pair<pll,ll>
+
+const int MOD = 1000000007;
+const char nl = '\n';
+
+int X[]={+0,+0,+1,-1,-1,+1,-1,+1};   // L, R, D, U, TR, BR, TL, BL
+int Y[]={-1,+1,+0,+0,+1,+1,-1,-1};  //
+
+int KX[]={-2,-2,-1,-1,1,1,2,2};  // Knight's Move
+int KY[]={-1,1,-2,2,-2,2,-1,1}; // Knight's Move
+
+int main()
+{
+    fastt;
+	ll t;
+	cin >> t;
+	while (t--) {
+		ll n;
+		cin >> n;
+		vll a(n);
+		for (auto &it : a) cin >> it;
+		vector<pair<ll, ll>> res;
+		int idx = -1;
+		for (ll i = 1; i < n; ++i) {
+			if (a[i] != a[0]) {
+				idx = i;
+				res.push_back({1, i + 1});
+			}
+		}
+		if (idx == -1) {
+			cout << "NO" << endl;
+			continue;
+		}
+		for (ll i = 1; i < n; ++i) {
+			if (a[i] == a[0]) {
+				res.push_back({idx + 1, i + 1});
+			}
+		}
+		cout << "YES" << endl;
+		for (auto x : res)
+        {
+            cout << x.first << " " << x.second << endl;
+        }       
+	}
+    return 0;
+}
