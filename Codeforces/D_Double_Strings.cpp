75ac0465e213254fa3342cd7b4459edfb7504213
Sabrina Akter <sabrinahossain202@gmail.com>
2023-09-19 16:59:56 +0600

    Codeforces/D_Double_Strings.cpp

---
+++ Codeforces/D_Double_Strings.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastt                \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define ll long long int
+#define vll vector<ll>
+#define pb push_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ms(x) memset(x, 0, sizeof x)
+#define pll pair<ll, ll>
+#define plll pair<pll, ll>
+
+const int MOD = 1000000007;
+const char nl = '\n';
+
+int X[] = {+0, +0, +1, -1, -1, +1, -1, +1}; // L, R, D, U, TR, BR, TL, BL
+int Y[] = {-1, +1, +0, +0, +1, +1, -1, -1}; //
+
+int KX[] = {-2, -2, -1, -1, 1, 1, 2, 2}; // Knight's Move
+int KY[] = {-1, 1, -2, 2, -2, 2, -1, 1}; // Knight's Move
+
+int main()
+{
+    fastt;
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s[n];
+        map<string, bool> mp;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> s[i];
+            mp[s[i]] = true;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            bool ok = false;
+            for (int j = 1; j < s[i].length(); j++)
+            {
+                string pref = s[i].substr(0, j), suff = s[i].substr(j, s[i].length() - j);
+                if (mp[pref] && mp[suff])
+                {
+                    ok = true;
+                }
+            }
+            cout << ok;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
