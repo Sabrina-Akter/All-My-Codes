5f6b0383d9fbc7e7f7c911b30c06f99591f81d4b
Sabrina Akter <sabrinahossain202@gmail.com>
2023-09-21 11:18:35 +0600

    Added files: GeeksforGeeks/Find all pairs with a given sum.cpp, Codeforces/A_Next_Test.cpp

---
+++ Codeforces/A_Next_Test.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastt ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long int
+#define vll vector <ll>
+#define pb push_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ms(x) memset(x, 0, sizeof x)
+#define pll pair<ll,ll>
+#define plll pair<pll,ll>
+
+const int MOD = 1000000007;
+const char nl = '\n';
+
+int X[]={+0,+0,+1,-1,-1,+1,-1,+1};   // L, R, D, U, TR, BR, TL, BL
+int Y[]={-1,+1,+0,+0,+1,+1,-1,-1};  //
+
+int KX[]={-2,-2,-1,-1,1,1,2,2};  // Knight's Move
+int KY[]={-1,1,-2,2,-2,2,-1,1}; // Knight's Move
+
+int main()
+{
+    fastt;
+    ll n, i, f=0, ans;
+    cin >> n;
+    vll v(n);
+    for(i=0;i<n;i++)
+    {
+        cin >> v[i];
+    }
+    sort(all(v));
+    for(i=1;i<=n;i++)
+    {
+        if(i!=v[i-1])
+        {
+            f=1;
+            ans = i;
+            break;
+        }
+    }
+    if(f==0)
+    {
+        ans = n+1;
+    }
+    cout << ans << nl;
+    return 0;
+}
---
+++ GeeksforGeeks/Find all pairs with a given sum.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+         vector<pair<int,int>> ans;
+         sort(A, A+N);
+         sort(B, B+M);
+         unordered_map<int, int> m;
+         int i;
+         for(i=0;i<M;i++)
+         {
+            m[B[i]]++;
+         }
+         for(i=0;i<N;i++)
+         {
+            int need = X - A[i];
+            if(m[need]>0)
+            {
+                ans.push_back({A[i], need});
+                m[need]--;
+            }
+         }
+         return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    int N, M, X;
+	    cin >> N >> M >> X;
+        int A[N], B[M];
+        
+	    for(int i = 0;i<N;i++)
+	        cin >> A[i];
+	    for(int i = 0;i<M;i++)
+	        cin >> B[i];
+	        
+	   Solution ob;
+	   vector<pair<int,int>> vp = ob.allPairs(A, B, N, M, X);
+	   int sz = vp.size();
+        if(sz==0)
+        cout<<-1<<endl;
+        else{
+            for(int i=0;i<sz;i++){
+                if(i==0)
+                cout<<vp[i].first<<" "<<vp[i].second;
+                else
+                cout<<", "<<vp[i].first<<" "<<vp[i].second;
+            }
+            cout<<endl;
+        }
+	}
+	return 0;
+}
+// } Driver Code Ends
