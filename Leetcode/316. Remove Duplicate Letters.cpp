0e8cd84fe39b28316e365b842d321cde513475b5
Sabrina Akter <sabrinahossain202@gmail.com>
2023-09-26 11:26:47 +0600

    Leetcode/316. Remove Duplicate Letters.cpp

---
+++ Leetcode/316. Remove Duplicate Letters.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+    string removeDuplicateLetters(string s) {
+        vector<int> lastIndex(26, 0);
+        for (int i = 0; i < s.length(); i++){
+            lastIndex[s[i] - 'a'] = i; 
+        }
+        
+        vector<bool> seen(26, false); 
+        stack<char> st;
+        
+        for (int i = 0; i < s.size(); i++) {
+            int curr = s[i] - 'a';
+            if (seen[curr]) continue; 
+            while(st.size() > 0 && st.top() > s[i] && i < lastIndex[st.top() - 'a']){
+                seen[st.top() - 'a'] = false; 
+                st.pop();
+            }
+            st.push(s[i]); 
+            seen[curr] = true; 
+        }
+        
+        string ans = "";
+        while (st.size() > 0){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+int main()
+{
+    string s;
+    cin >> s;
+    removeDuplicateLetters(s);
+
+    return 0;
+}
