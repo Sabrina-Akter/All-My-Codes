e66aaf7bdf5b7de0099a62e8ab76732424835bbc
Sabrina Akter <sabrinahossain202@gmail.com>
2023-11-07 10:24:18 +0600

    GeeksforGeeks/Remove Duplicates.cpp

---
+++ GeeksforGeeks/Remove Duplicates.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	string removeDups(string S) 
+	{
+	    vector <int> count(26);
+	    string ans;
+	    for(int i = 0; i < S.size(); i++)
+	    {
+	        if(count[S[i] - 'a'] == 1) continue;
+	        count[S[i] - 'a'] = 1;
+	        ans += S[i];
+	    }
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   		
+   		Solution ob;
+   		cout << ob.removeDups(s) << "\n";
+   		
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
