684091488b4eeaf90fa38fc3d17b0eaffd39ee01
Sabrina Akter <sabrinahossain202@gmail.com>
2024-02-17 13:49:32 +0600

    GeeksforGeeks/Check if a string is Isogram or not.cpp

---
+++ Codeforces/C_Banknotes.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastt ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long int
+#define vll vector <ll>
+#define pb push_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ms(x) memset(x, 0, sizeof x)
+#define pll pair<ll,ll>
+#define plll pair<pll,ll>
+
+const int MOD = 1000000007;
+const char nl = '\n';
+
+int X[]={+0,+0,+1,-1,-1,+1,-1,+1};   // L, R, D, U, TR, BR, TL, BL
+int Y[]={-1,+1,+0,+0,+1,+1,-1,-1};  //
+
+int KX[]={-2,-2,-1,-1,1,1,2,2};  // Knight's Move
+int KY[]={-1,1,-2,2,-2,2,-1,1}; // Knight's Move
+
+int main()
+{
+    fastt;
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n, k, x;
+        cin >> n >> k;
+        vll ar(n+4);
+        for(ll i = 0; i < n; i++)
+        {
+            cin >> x;
+            ar[i] = pow(10, x);
+        }
+        if(n == 1) cout << k + 1 << nl;
+        else
+        {
+            ll ans=0;
+            ll x;
+            k++;
+            for(ll i = 0; i < n - 1; i++)
+            {
+                if(ar[i + 1] % ar[i] == 0)
+                {
+                    x = ar[i + 1] / ar[i] - 1;
+                }
+                else x = ar[i + 1] / ar[i];
+                if(x >= k)
+                {
+                    ans += k * ar[i];
+                    k = 0;
+                    break;
+                }
+                else
+                {
+                    ans += x * ar[i];
+                    k -= x;
+                }
+            }
+ 
+            if(k)
+            {
+                ans += k * ar[n - 1];
+            }
+            cout << ans << nl;
+ 
+        }
+    }
+    return 0;
+}
