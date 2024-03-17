fd649c8df9a735a1a6a12a9b7865178c101b411a
Sabrina Akter <sabrinahossain202@gmail.com>
2024-03-17 12:19:51 +0600

    Leetcode/57. Insert Interval.cpp

--- Leetcode/57. Insert Interval.cpp
+++ Leetcode/57. Insert Interval.cpp
@@ -1,47 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> ans;
+class Solution 
+{
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
+    {
         intervals.push_back(newInterval);
         sort(intervals.begin(), intervals.end());
-        map <int, int> m;
-        int i, a, b, mn = INT32_MAX, mx = INT32_MIN;
-        for(i=0;i<intervals.size();i++)
+        vector<vector<int>> ans;
+        for(int i = 0; i < intervals.size(); i++)
         {
-            a = intervals[i][0], b = intervals[i][1];
-            if(a>=mn && a<=mx)
+            if(ans.empty() || ans.back()[1] < intervals[i][0])
             {
-                mn = min(a, mn);
+                vector <int> temp = {intervals[i][0], intervals[i][1]};
+                ans.push_back(temp);
             }
             else
             {
-                mn = a;
+                ans.back()[0] = min(ans.back()[0], intervals[i][0]);
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-            if(b>=mn && b<=mx)
-            {
-                mx = max(b, mx);
-            }
-            else
-            {
-                mx = b;
-            }
-            m[mn] = mx;
-        }
-        for(auto it=m.begin(); it!=m.end(); it++)
-        {
-            vector <int> n;
-            n.push_back((*it).first);
-            n.push_back((*it).second);
-            ans.push_back(n);
         }
         return ans;
+
     }
+};
 
 int main()
 {
-    vector<vector<int>> v = {{4,6}};
-    vector<int> i = {1,2};
-    insert(v, i);
     return 0;
 }
