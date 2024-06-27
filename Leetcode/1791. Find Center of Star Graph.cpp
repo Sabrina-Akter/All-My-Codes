181af7fa14c2f35fcb8dbaa01bc1b110bf2c4010
Sabrina <sabrinahossain202@gmail.com>
2024-06-27 11:26:48 +0600

    Leetcode/1791. Find Center of Star Graph.cpp

--- Leetcode/1791. Find Center of Star Graph.cpp
+++ Leetcode/1791. Find Center of Star Graph.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findCenter(vector<vector<int>>& edges) {
-      int i, A[4], ans;
-      map <int, int> m;
-      A[0] = edges[0][0], A[1] = edges[0][1], A[2] = edges[1][0], A[3] = edges[1][1];
-      for(i=0;i<4;i++)
-      {
-        m[A[i]]++;
-        if(m[A[i]]==2)
-        {
-            ans = A[i];
-        }
-      }
-      return ans;
+int findCenter(vector<vector<int>>& edges) 
+{
+    unordered_map <int, int> freq;
+    int ans;
+    for(int i = 0; i < edges.size(); i++)
+    {
+        freq[edges[i][0]]++;
+        freq[edges[i][1]]++;
+        if(freq[edges[i][0]] == edges.size()) ans = edges[i][0];
+        if(freq[edges[i][1]] == edges.size()) ans = edges[i][1];
     }
+    return ans;
+}
 
 int main()
 {
