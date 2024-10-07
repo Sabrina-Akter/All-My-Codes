c44bb7291bd9f22074ae9111d12c134a93a08a14
Sabrina <sabrinahossain202@gmail.com>
2024-10-07 18:38:44 +0600

    Leetcode/2696. Minimum String Length After Removing Substrings.cpp

---
+++ Leetcode/2696. Minimum String Length After Removing Substrings.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minLength(string s) 
+{
+    stack <char> st;
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == 'B' && st.size() > 0 && st.top() == 'A')
+        {
+            st.pop();
+        }
+        else if(s[i] == 'D' && st.size() > 0 && st.top() == 'C')
+        {
+            st.pop();
+        }
+        else st.push(s[i]);
+    }
+    return st.size();
+}
+
+int main()
+{
+    
+
+    return 0;
+}
