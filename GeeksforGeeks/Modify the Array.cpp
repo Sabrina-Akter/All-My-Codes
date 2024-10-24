c147a789ed5a27fcfe677487d30a4e07895dd817
Sabrina <sabrinahossain202@gmail.com>
2024-10-24 11:01:06 +0600

    GeeksforGeeks/Modify the Array.cpp

---
+++ GeeksforGeeks/Modify the Array.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution 
+{
+  public:
+    vector<int> modifyAndRearrangeArray(vector<int> &arr) 
+    {
+        int n = arr.size();
+        for(int i = 1; i < n; i++)
+        {
+           if(arr[i] == arr[i - 1]) 
+           {
+               arr[i] = 0;
+               arr[i - 1] *= 2;
+           }
+        }
+        int x = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(arr[i] != 0)
+            {
+                swap(arr[x], arr[i]);
+                x++;
+            }
+        }
+        return arr;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.modifyAndRearrangeArray(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
