bb9b3168708a2b54a51bb1adfbb5b16fd2657c54
Sabrina <sabrinahossain202@gmail.com>
2025-02-20 09:40:26 +0600

    Leetcode/1980. Find Unique Binary String.cpp

--- Leetcode/1980. Find Unique Binary String.cpp
+++ Leetcode/1980. Find Unique Binary String.cpp
@@ -1,35 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution 
+bool f(string s, int n, unordered_map<string, int> &found, string &ans) 
 {
-public:
-    int size;
-    unordered_map <string, int> mp;
-    string ans;
-    void func(string s)
-    {
-        if(s.size() == size) 
+    if(s.size() == n) 
+    {   
+        if(found[s] == 0) 
         {
-            if(mp.count(s) == false) ans = s;
-            return;
+            ans = s;
+            return true;  
         }
-        else
-        {
-            func(s + '0');
-            func(s + '1');
-        }
-        return;
+        return false;
     }
+    
+    s.push_back('0');
+    if(f(s, n, found, ans)) return true;  
+    
+    s.back() = '1';  
+    if(f(s, n, found, ans)) return true;  
+    
+    s.pop_back(); 
+    return false;
+}
 
-    string findDifferentBinaryString(vector<string>& nums) 
-    {
-        for(auto &it:nums) mp[it] = 1;
-        size = nums[0].size();
-        func("");
-        return ans;
-    }
-};
+string findDifferentBinaryString(vector<string>& nums) 
+{
+    unordered_map<string, int> found;
+    for(string &s : nums) found[s] = 1;
+    string ans;
+    f("", nums.size(), found, ans);
+    return ans;
+}
 
 int main()
 {
