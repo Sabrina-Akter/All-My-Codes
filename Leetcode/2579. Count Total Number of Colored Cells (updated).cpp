6d44803758c328a3f5a103a61aeb9bd3f5330e64
Sabrina <sabrinahossain202@gmail.com>
2025-03-05 12:50:19 +0600

    Leetcode/2579. Count Total Number of Colored Cells (updated).cpp

---
+++ Leetcode/2579. Count Total Number of Colored Cells (updated).cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long coloredCells(int n) 
+{
+    long long a = n;
+    long long b = n - 1;
+    return (a * a) + (b * b);
+}
+
+int main()
+{
+    
+
+    return 0;
+}
