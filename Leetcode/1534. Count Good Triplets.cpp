4a2df5a404df8fcb6925a92299b1d5cb02f43fc6
Sabrina <sabrinahossain202@gmail.com>
2025-04-14 13:42:01 +0600

    Leetcode/1534. Count Good Triplets.cpp

---
+++ Leetcode/1534. Count Good Triplets.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countGoodTriplets(vector<int>& arr, int a, int b, int c) 
+{
+    int n = arr.size(), ans = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(abs(arr[i] - arr[j]) <= a)
+            {
+                for(int k = j + 1; k < n; k++)
+                {
+                    if((abs(arr[j] - arr[k]) <= b) && (abs(arr[i] - arr[k]) <= c)) ans++;
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    
+
+    return 0;
+}
