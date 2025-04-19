59b02e644754b65355fc9118e0ace2c1e7dcb17a
Sabrina <sabrinahossain202@gmail.com>
2025-04-19 16:19:30 +0600

    Leetcode/2563. Count the Number of Fair Pairs.cpp

--- Leetcode/2563. Count the Number of Fair Pairs.cpp
+++ Leetcode/2563. Count the Number of Fair Pairs.cpp
@@ -4,20 +4,15 @@ using namespace std;
 long long countFairPairs(vector<int>& nums, int lower, int upper) 
 {
     sort(nums.begin(), nums.end());
-    long long ans = 0;
-    int x1, x2;
-    for(int i = 0; i < nums.size(); i++)
+    long long count = 0;
+    int n = nums.size();
+    for (int i = 0; i < n; ++i)
     {
-        x1 = lower - nums[i];
-        x2 = upper - nums[i];
-        auto it1 = lower_bound(nums.begin() + i + 1, nums.end(), x1);
-        auto it2 = upper_bound(nums.begin() + i + 1, nums.end(), x2);
-        it2--;
-        x1 = it1 - nums.begin();
-        x2 = it2 - nums.begin();
-        if(x1 <= x2) ans += (x2 - x1) + 1;
+        int left = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
+        int right = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
+        count += (right - left);
     }
-    return ans;
+    return count;   
 }
 
 int main()
