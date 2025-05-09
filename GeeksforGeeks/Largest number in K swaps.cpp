6d3b198974dd60d68c81b414ef6d04cba6d4370e
Sabrina <sabrinahossain202@gmail.com>
2025-05-09 13:58:56 +0600

    GeeksforGeeks/Largest number in K swaps.cpp

---
+++ GeeksforGeeks/Largest number in K swaps.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    string findMaximumNum(string& s, int k) 
+    {
+        if (k == 0) 
+        {
+            return s;
+        }
+        int n = s.size();
+        string ans = s;
+        for (int i = 0; i < n - 1; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+            {
+                if (s[i] < s[j]) 
+                {
+                    swap(s[i], s[j]);
+                    ans = max(ans, findMaximumNum(s, k - 1));
+                    swap(s[i], s[j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t, k;
+    string str;
+
+    cin >> t;
+    while (t--) {
+        cin >> k >> str;
+        Solution ob;
+        cout << ob.findMaximumNum(str, k) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
