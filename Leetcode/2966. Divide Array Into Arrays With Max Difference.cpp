4d8b6a1a4a0d820784b35f1a055bb5fb363ca912
Sabrina <sabrinahossain202@gmail.com>
2025-06-18 21:06:36 +0600

    Leetcode/2966. Divide Array Into Arrays With Max Difference.cpp

--- Leetcode/2966. Divide Array Into Arrays With Max Difference.cpp
+++ Leetcode/2966. Divide Array Into Arrays With Max Difference.cpp
@@ -1,35 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution 
+vector<vector<int>> divideArray(vector<int>& nums, int k) 
 {
-public:
-    vector<vector<int>> divideArray(vector<int>& nums, int k) 
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    vector <int> x;
+    for(int i = 0; i < nums.size(); i++)
     {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-        vector <int> temp;
-        for(int i = 0; i < nums.size(); i++)
+        x.push_back(nums[i]);
+        if((i + 1) % 3 == 0)
         {
-            if(i % 3 == 0 && i != 0)
-            {
-                ans.push_back(temp);
-                temp.clear();
-            }
-            temp.push_back(nums[i]);
+            ans.push_back(x);
+            x.clear();
         }
-        ans.push_back(temp);
-        for(auto &it : ans)
-        {
-            if(it[2] - it[0] > k)
-            {
-                ans.clear();
-                break;
-            }
-        }
-        return ans;
     }
-};
+    cout << ans.size() << endl;
+    for(int i = 0; i < ans.size(); i++)
+    {
+        if(ans[i][2] - ans[i][0] > k)
+        {
+            ans.clear();
+            break;
+        }
+    }
+    return ans;
+}
 
 int main()
 {
