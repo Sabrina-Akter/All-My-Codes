f38b66fc7f4f8ce46eb162a63c305e438cb181a7
Sabrina <sabrinahossain202@gmail.com>
2025-07-09 22:10:37 +0600

    modified

---
+++ Codeforces/C_Ehab_and_a_2_operation_task.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, v = 1e6, i;
+
+int main() 
+{
+    cin >> n;
+    cout << n + 1 << '\n';
+    cout << "2 " << n << " 1\n";
+    cout << "1 " << n << " " << v << '\n';
+    while (--n) 
+    {
+        cout << "2 " << ++i << " " << --v << '\n';
+    }
+    return 0;
+}
